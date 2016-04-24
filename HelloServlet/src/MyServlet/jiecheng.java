1fb41d4cdcbaf4f65c904e305cfd8a6304f618f5
Vczzz <854590736@qq.com>
2016-04-25 00:25:29 +0800

    1

---
+++ HelloServlet/src/MyServlet/jiecheng.java
@@ -0,0 +1,81 @@
+package MyServlet;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import bean.FactorialBean;
+
+public class jiecheng extends HttpServlet {
+
+	/**
+	 * Constructor of the object.
+	 */
+	public jiecheng() {
+		super();
+	}
+
+	/**
+	 * Destruction of the servlet. <br>
+	 */
+	public void destroy() {
+		super.destroy(); // Just puts "destroy" string in log
+		// Put your code here
+	}
+
+	/**
+	 * The doGet method of the servlet. <br>
+	 *
+	 * This method is called when a form has its tag value method equals to get.
+	 * 
+	 * @param request the request send by the client to the server
+	 * @param response the response send by the server to the client
+	 * @throws ServletException if an error occurred
+	 * @throws IOException if an error occurred
+	 */
+	public void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		doPost(request,response);
+	}
+
+	/**
+	 * The doPost method of the servlet. <br>
+	 *
+	 * This method is called when a form has its tag value method equals to post.
+	 * 
+	 * @param request the request send by the client to the server
+	 * @param response the response send by the server to the client
+	 * @throws ServletException if an error occurred
+	 * @throws IOException if an error occurred
+	 */
+	public void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		int m,f = 1;
+		FactorialBean fac  = new FactorialBean();
+		request.setAttribute("myFact", fac);
+		m = Integer.parseInt(request.getParameter("number"));
+		fac.setMult(m);
+		for(int i =1;i <= m;i++){
+			f = f * i;
+		}
+		fac.setFact(f);
+		RequestDispatcher dispatcher = request.getRequestDispatcher("shuchu.jsp");
+		dispatcher.forward(request, response);
+	}
+
+	/**
+	 * Initialization of the servlet. <br>
+	 *
+	 * @throws ServletException if an error occurs
+	 */
+	public void init() throws ServletException {
+		// Put your code here
+	}
+
+}
---
+++ HelloServlet/src/bean/FactorialBean.java
@@ -0,0 +1,23 @@
+package bean;
+
+public class FactorialBean {
+
+	int mult,fact;
+
+	public int getMult() {
+		return mult;
+	}
+
+	public void setMult(int mult) {
+		this.mult = mult;
+	}
+
+	public int getFact() {
+		return fact;
+	}
+
+	public void setFact(int fact) {
+		this.fact = fact;
+	}
+	
+}
